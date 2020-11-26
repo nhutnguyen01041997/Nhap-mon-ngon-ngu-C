7ea66717d7763a6eb5fd1b716a0abae478e1dddf
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2020-11-26 14:15:27 +0700

    Add files via upload

---
+++ Bài tập 1/pheptinh.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+	//Khai bao bien a, b
+    int a, b;
+    a = 3;
+    b = 5;
+    //khai bao bien tong, hieu, tich, thuong
+    int tong , hieu, tich, thuong;
+    tong = a + b;
+    hieu = a - b;
+    tich = a * b;
+    thuong = a / b;
+    
+    printf(" tong = %d\n ", tong);
+    
+    printf(" thuong = %d\n", hieu);
+    
+    printf(" tich = %d\n", tich);
+    
+    printf(" thuong = %d\n", thuong);
+    
+    
+    double c = 3;
+    double d = 5;
+     printf("thuong = %0.1f\n", c/d);
+	
+	getchar();
+	return 0;
+}
