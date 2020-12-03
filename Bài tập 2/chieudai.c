57d66b615da4c48b92a72c097a52b031f92fc0ba
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2020-12-03 15:31:30 +0700

    Update chieudai.c

--- Bài tập 2/chieudai.c
+++ Bài tập 2/chieudai.c
@@ -3,14 +3,20 @@
 int main(void) {
   // khai bao bien va nhap du lieu tu ban phim
   double l;
+  
+  
   // khai bao don vi chieu dai m, cm, mm
   double m, cm, mm;
+  m=l*1;
+  cm=l*100;
+  mm=l*1000;
   
+ 
   
   printf("nhap l = ");// nhap l tu ban phim
   scanf("%lf",&l);
-  printf("m\t cm\t mm\t \n ");
-  printf("%lf\t %lf\t %lf\t \n", m, cm,mm);
+  printf("m\t cm\t mm\t  \n ");
+  printf("%lf\t %lf\t %lf\t \n", m, cm,mm  );
    
   getchar();
   return 0;
