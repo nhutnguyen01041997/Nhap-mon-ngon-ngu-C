f1489c52734ca6196b03adc96198e6c26009783f
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2020-12-03 14:03:51 +0700

    Add files via upload

---
+++ Bài tập 2/a+b.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	//khai bao bien a, b
+	int a, b;
+    printf("please kick a =");
+	scanf("%d",&a);
+	printf("please kick b =");
+	scanf("%d",&b);
+	printf("a + b = %d", a + b);
+	getchar ();
+	return 0;  
+}
