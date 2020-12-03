1ce29ad4cab15dbcb055a2d4a975d7b47d93ceae
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2020-12-03 16:40:30 +0700

    Update ketquahoctap.c

--- Bài tập 2/ketquahoctap.c
+++ Bài tập 2/ketquahoctap.c
@@ -5,7 +5,7 @@ int main(void) {
   double V,T;
   //khai bao bien Diem Trung Binh(DTB)
   double DTB;
-  DTB=(V+T)/2;
+  DTB=(V + T)/2;
   printf("Nhap Diem Van V = ");//nhap tu ban phim
   scanf("%lf",&V);
   printf("Nhap Diem Toan T = ");//nhap tu ban phim
@@ -13,7 +13,7 @@ int main(void) {
   //Hien thi tieu de
   printf("Diem Van(V)\t Diem Toan(T)\t Diem Trung Binh(DTB)\t \n");
   //Hien thi ket qua
-  printf("%.2f\t         %.2f\t          %.2f\t \n",V, T, (V+T)/2);
+  printf("%.2f\t         %.2f\t          %.2f\t \n",V, T, (V + T)/2);
 
   getchar();
   return 0;
