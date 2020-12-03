475af60f09a18923888d85a9d75eec944fa969bf
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2020-12-03 15:11:58 +0700

    Add files via upload

---
+++ Bài tập 2/chieudai.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  // khai bao bien va nhap du lieu tu ban phim
+  double l;
+  // khai bao don vi chieu dai m, cm, mm
+  double m, cm, mm;
+  
+  
+  printf("nhap l = ");// nhap l tu ban phim
+  scanf("%lf",&l);
+  printf("m\t cm\t mm\t \n ");
+  printf("%lf\t %lf\t %lf\t \n", m, cm,mm);
+   
+  getchar();
+  return 0;
+}
