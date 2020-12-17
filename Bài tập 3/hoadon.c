9a9afa3c5823702692637ba1895f22df705dedb2
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2020-12-17 13:43:11 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 int main (){
 	//khai bao bien
   double dongia,soluong,thanhtien ;
@@ -12,12 +10,13 @@ int main (){
   thanhtien = dongia * soluong;
   printf("thanhtien \n");
   printf("%.1f",thanhtien);
-  }else if (soluong>=5){
-  thanhtien = dongia*soluong*0.75;// giam gia 25% neu mua so luong tu 5 tro len
+  }else if (soluong>= 5 && dongia >= 500){
+  thanhtien = dongia*soluong*0.5;// giam gia 50% neu mua so luong tu 5 và có giá từ 500 tro len
   printf("thanhtien \n");
   printf ("%.1f",thanhtien );
   }
   getchar ();
   return 0;
 }
+
 
