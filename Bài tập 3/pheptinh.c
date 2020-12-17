2a01b12afc7c3935d63f60c0277d52470d50c637
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2020-12-17 15:59:13 +0700

    Update pheptinh.c

--- Bài tập 3/pheptinh.c
+++ Bài tập 3/pheptinh.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+
+int main (){
+  //khai bao bien va gan gia tri
+int a =5, b = 2;
+// khai bao bien pt kieu char duoc nhap tu ban phim
+char pt;
+printf("nhap phep tinh =");
+scanf("%c", &pt);
+//su dung cau lenh switch de hien thi phep tinh trong phuong trinh 
+switch(pt){
+  case '+':
+  printf("%d", a + b);
+  break;
+  case '-':
+  printf("%d",a - b );
+  break;
+  case '*':
+  printf("%d", a * b);
+  break;
+  case '/':
+  printf("%d", a / b);
+    //nhac nho khi nhap sai
+    default:
+    printf("dau vao khong hop le, vui long nhap +, -, *, /");
+  }
+
+
+     
+
+  getchar ();
+  return 0;
+}
 
