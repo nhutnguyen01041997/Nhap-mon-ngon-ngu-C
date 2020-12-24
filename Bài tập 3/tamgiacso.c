7ef821875cdfedd180346c0caef8fa19a1bc1ad0
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2020-12-24 21:26:36 +0700

    Update tamgiacso.c

--- Bài tập 3/tamgiacso.c
+++ Bài tập 3/tamgiacso.c
@@ -6,12 +6,12 @@ int main()
     //khai bao bien n và hien thi tu ban phim
     printf("Nhap n:");
     scanf("%d", &n);
-    //kiem tra dieu kien cua n
+    //kiem tra dieu kien cua n va in ra tam giac so
     for(int i = 1; i <= n; i++){
        for(int j = 1; j <= n; j++)
           printf(" ");
        for(int x = 1; x <= i; x++)
-          printf("%d",x);// in ra tam giác số
+          printf("%d", x);
         printf("\n");
        
     }
