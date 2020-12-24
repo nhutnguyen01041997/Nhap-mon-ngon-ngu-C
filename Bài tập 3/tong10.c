af6fccbbfd478622f6aeaa987c4c53295a58779a
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2020-12-24 14:44:31 +0700

    Create tong10.c

---
+++ Bài tập 3/tong10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+ 
+ int sum = 0;
+ int count;
+ for( count = 1; count <= 10; count++){
+   sum = sum + count;
+   }
+  printf("tong tu 1 den muoi = %d", sum);
+  return 0;
+  }
