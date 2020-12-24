ce5d02f7ca3b59862d30552f8d20b0653ced9c64
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2020-12-24 13:50:00 +0700

    Create xinchao.c

---
+++ Bài tập 3/xinchao.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  //khai báo biến số đếm
+  int count;
+  // in ra 10 dòng xin chào
+  for(count = 1; count <= 10; count++){
+    printf("\n xinchao");
+    }
+  
+  
+  return 0;
+}
