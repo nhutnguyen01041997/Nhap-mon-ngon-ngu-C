74a0af8a6ab0c789c481233d1782088c0d0dcb1f
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2021-01-07 17:09:29 +0700

    Create bangcuuchuong2.c

---
+++ Bài tập 2/bangcuuchuong2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+ int a = 2, b = 3, c = 4;
+ printf("bang cuu chuong 2\n");
+  
+ for(int i = 1; i <= 3 ; i++){
+   printf("%d * %d= %d\n",a, i, a*i);
+  }
+  printf("bang cuu chuong 3\n");
+  for(int i = 1; i <= 3 ; i++){
+   printf("%d * %d= %d\n",b, i, b*i);
+  }
+  printf("bang cuu chuong 4\n");
+  for(int i = 1; i <= 3 ; i++){
+   printf("%d * %d= %d\n",c, i, c*i);
+  }
+
+ 
+ 
+  return 0;
+}
+    
+    
