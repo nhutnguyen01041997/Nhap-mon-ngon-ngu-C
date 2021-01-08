66d29301c8f11a8f99d2bbabe90fd957b623c419
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2021-01-08 09:43:04 +0700

    Update kt_04.c

--- Bài tập 3/kt_04.c
+++ Bài tập 3/kt_04.c
@@ -39,14 +39,30 @@ int main(void) {
   printf("nhap so nguyen n =");
   scanf("%d", &n);
   for( ; n > 0; i++){
+  i = n % 10;//n chia cho 10 lấy số dư (VD:123/10=12.3(dư 3)
+  S += i; 
+  n /= 10;
+
+  printf("%d\n", S);
+  }
+  return 0;
+}
+
+Vòng lặp while
+#include <stdio.h>
+
+int main(void) {
+  
+  int n;
+  int S = 0;
+  int i;
+  printf("nhap so nguyen n =");
+  scanf("%d", &n);
+  while(n != 0){
   i = n % 10;
   S += i;
   n /= 10;
- 
- 
-
-   
-
+  
   printf("%d\n", S);
   }
   return 0;
