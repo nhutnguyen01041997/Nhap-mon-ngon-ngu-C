6ae9742676995ea8181d76e685343a06bebd79f7
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2021-01-14 10:00:11 +0700

    Create kt_08.c

---
+++ Bài tập 3/kt_08.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+// khoi tao bien
+  float a  , b ;
+// nhap a, b tu ban phim
+  printf("nhap so thuc a :");
+  scanf("%f", &a);
+  printf("nhap so thuc b :");
+  scanf("%f", &b);
+ // khoi tao cong thuc
+  float c = a * b;
+  // xet dieu kien 
+  if(c > 0){
+    printf(" %.f va %.f cung dau", a, b);
+    }else if( c < 0){
+      printf(" %.f va %.f trai dau", a, b);
+    }else{
+      if(a == 0 && b == 0){
+        printf("khong xac dinh vi a va b = 0");
+    }else if(a == 0){
+      printf("khong xac dinh vi a  bang khong");
+      }else{
+        printf("khong xac dinh vi b bang 0");
+      }
+    }  
+  return 0;
+}
