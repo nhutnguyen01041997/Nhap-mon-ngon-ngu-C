675d950b27d4bdff00cbff7d37342e615e2d67fc
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2021-01-14 15:29:41 +0700

    Update mang_01.c

--- Bài tập 4/mang_01.c
+++ Bài tập 4/mang_01.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
-  int arr[4];
+  // khai bao mang
+  int arr[4]={3, 5, 7,2};
   int S = 0;
+  //tao vong lap hien thi cac so trong mang
   for(int i = 0; i < 4; i++){
-    printf("nhap mang array :");
-    scanf("%d", &arr[i]);
+  printf(" %d\n" , arr[i]);
+  }
+  // Tao vong lap hien thi tong cac so trong mang
+  for(int i = 0; i < 4; i++){
     S += arr[i];
-    }
-    printf("%d" , S);
-    
-
+  }
+  // hien thi ra man hinh ket qua
+    printf("tong cua mang %d" , S);
+  
+  
   return 0;
 }
