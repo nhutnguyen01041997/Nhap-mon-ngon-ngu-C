26ff905162d95a401f4db40adbf6d64baecd6b55
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2021-01-21 21:45:56 +0700

    Update mang_05.c

--- Bài tập 4/mang_05.c
+++ Bài tập 4/mang_05.c
@@ -6,22 +6,32 @@ int main(void) {
   scanf("%d", &n);
   
   int a[n];
+  //nhap phan tu n cua mang a tu ban phim
   for(int i = 0; i < n; i++){
     printf("nhap phan tu so %d :", i+1);
     scanf("%d", &a[i]);
   }
+  // hien thi cac phan tu cua mang a
   printf("\nmang a :");
   for(int i = 0; i < n; i++){
     printf(" %d", a[i]);
     }
+  // nhap so nguyen m tu ban phim
     printf("\nnhap so nguyen m :");
     scanf("%d", &m);
+  //kiem tra phan tu m co ton tai trong mang a hay khong neu 
+  int j;
   for(int i = 0; i < n; i++ ){
-    if(a[i] == m)
+    if(a[i] == m){
+      j+=1;
+      }
+    }
+  if(j>0){
     printf("\nco ton tại");
-    
-  if(a[i]!=m)
-  printf("\nkhong ton tai");
+    }
+  else
+    printf("\nKhong ton tai")
+  
   
 
   return 0;
