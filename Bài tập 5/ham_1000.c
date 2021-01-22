7c9a5a80c6d941fafd6fe9c6da112f7e9f2e4eb4
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2021-01-22 15:23:20 +0700

    Update ham_1000.c

--- Bài tập 5/ham_1000.c
+++ Bài tập 5/ham_1000.c
@@ -1,6 +1,6 @@
 // khoi khai bao
 #include <stdio.h>
-
+//Khai báo biến toàn cục
 int x = 1, y = 2; 
 
 void ham_a(int x);
@@ -9,8 +9,8 @@ void ham_b();
 // khoi ham main
 int main(void) {
   int x = 3;
-  printf(" %d", x);//kết quả 3
-  printf(" %d", y);// kết qua 3
+  printf(" %d", x);//kết quả 3 , do khai báo biến x trong main là 3
+  printf(" %d", y);// kết qua 2
   ham_a(x);
   ham_b();
   return 0;
@@ -19,7 +19,7 @@ int main(void) {
 // trả về giá trị hàm a
 void ham_a(int x){
   x = 5;
-  printf(" %d", x);//kết quả 5
+  printf(" %d", x);//kết quả 5, do khai bao bien x trong main là 5
 }
 //trả về giá trị hàm b
 void ham_b(){
