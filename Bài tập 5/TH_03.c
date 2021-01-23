9c5a7bdd217fd473fb0e895028ac1627af0f21f1
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2021-01-23 09:26:32 +0700

    Update TH_03.c

--- Bài tập 5/TH_03.c
+++ Bài tập 5/TH_03.c
@@ -1,23 +1,23 @@
 //khoi khai bao
 #include<stdio.h>
-int a, b, c;
-int so_lon_nhat();
+float a, b, c;
+float so_lon_nhat();
 //khoi ham main
 int main (){
   //khai báo biến
   
   // nhập a, b, c từ bàn phím
   printf("nhap a =");
-  scanf("%d", &a);
+  scanf("%f", &a);
   printf("nhap b =");
-  scanf("%d", &b);
+  scanf("%f", &b);
   printf("nhap c =");
-  scanf("%d", &c);
+  scanf("%f", &c);
   so_lon_nhat();
   return 0;
   }
   //khoi dinh nghia ham
-  int so_lon_nhat(){
+  float so_lon_nhat(){
   if(a>b && a>c){
   printf("a lon nhat");
 } else if( b>a && b>c){
